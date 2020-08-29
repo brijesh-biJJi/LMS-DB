a964a0a509ee9284f3c77e5c5a608b8c7ff5d316
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-08-29 14:14:01 +0530

    [Brijesh] Updated LMS DB Queries

--- lms-db.sql
+++ lms-db.sql
@@ -1,14 +1,10 @@
 1
 SELECT *
 FROM Fellowship_Candidate as f
-LEFT JOIN Candidate_Qualification_details as q
-ON f.candidate_id = q.candidate_id
 LEFT JOIN Candidate_Stack as s
 ON f.candidate_id = s.candidate_id
 LEFT JOIN Requirement as r
 ON s.requirement_id = r.requirement_id
-LEFT JOIN Company as c
-ON c.company_id = r.company_id
 LEFT JOIN Tech_Stack as t
 ON t.tech_stack_id = r.tech_stack_id
 WHERE t.tech_name='Java';
@@ -61,6 +57,13 @@ LEFT JOIN Candidate_Qualification_Details as cq
 ON f.candidate_id = cq.candidate_id
 WHERE cq.passing_year = 2019;
 
+14
+SELECT *
+FROM Fellowship_Candidate as fc
+LEFT JOIN Candidate_Qualification_Details cq
+ON fc.candidate_id = cq.candidate_id
+WHERE cq.degree_name = 'MCA';
+
 16
 SELECT ROUND(DATEDIFF(CURDATE(),joinig_Date)/7) as weeks_completed_since_joining
 FROM Fellowship_Candidate
@@ -85,6 +88,36 @@ LEFT JOIN Candidate_Stack as cs
 ON fc.candidate_id = cs.candidate_id
 WHERE fc.candidate_id=6;
 
+20
+SELECT * 
+FROM Fellowship_Candidate f 
+LEFT JOIN 
+Candidate_Stack cs 
+ON f.candidate_id = cs.candidate_id 
+WHERE cs.complete_date IS NOT NULL;
+
+21
+SELECT f.candidate_id,f.first_name, f.last_name, f.email, c.company_name
+FROM Fellowship_Candidate as f
+LEFT JOIN Candidate_Stack as cs
+ON f.candidate_id = cs.candidate_id
+LEFT JOIN Requirement as r
+ON cs.requirement_id = r.requirement_id
+LEFT JOIN company as c
+ON r.company_id = c.company_id
+WHERE c.company_id IS NOT NULL;
+
+22
 
 
+23
+SELECT  f.first_name,f.last_name, r.buddy_engg_id,r.ideation_engg_id,t.tech_name
+FROM Fellowship_Candidate as f
+LEFT JOIN Candidate_Stack as cs
+ON f.candidate_id = cs.candidate_id
+LEFT JOIN Requirement as r
+ON cs.requirement_id = r.requirement_id
+LEFT JOIN Tech_Stack as t
+ON t.tech_stack_id = r.tech_stack_id
+where r.requirement_id is not null;
 
